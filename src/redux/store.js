599662eb765eeb8fa126b2a4e5ee416a753f8cbc
Elena Solomko <lena06051981be@gmail.com>
2023-01-19 09:01:56 +0200

    up

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,43 +1,57 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import storage from 'redux-persist/lib/storage';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
+import { configureStore } from '@reduxjs/toolkit'
+// import { combineReducers, configureStore } from '@reduxjs/toolkit'
+// import storage from 'redux-persist/lib/storage';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
 import { filtersReducer } from './filterSlice';
 import { contactsReducer } from './contactsSlice';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['contacts']
-  //     or
-  // blacklist: ['filter'],
-};
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+//   filter: filtersReducer,
+// });
 
-
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filtersReducer,
-});
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// export const store = configureStore({
+//   reducer: rootReducer,
+// });
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  
-  middleware: getDefaultMiddleware =>
-  getDefaultMiddleware({
-    serializableCheck: {
-      ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-})
+  reducer: {
+    contacts: contactsReducer,
+    filter: filtersReducer,
+  },
+});
 
-export const persistor = persistStore(store);
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   whitelist: ['contacts']
+//   //     or
+//   // blacklist: ['filter'],
+// };
+
+
+
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+  
+//   middleware: getDefaultMiddleware =>
+//   getDefaultMiddleware({
+//     serializableCheck: {
+//       ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//     },
+//   }),
+// })
+
+// export const persistor = persistStore(store);
